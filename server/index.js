9cc2b81d3f9ad5e1bbfbf7606f9552e592616bb0
Andreas Kohn <andreas.kohn@gmail.com>
2016-08-11 10:26:21 +0200

    Silence warnings about a 'missing' .env file
    In production the environment is given via environment variables (heroku configuration variables),
    and we do not have nor want an .env file there.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+require('dotenv').config({ path: path.resolve(__dirname, '../.env'), silent: true });
 
 const fs = require('fs');
 const pg = require('pg');
